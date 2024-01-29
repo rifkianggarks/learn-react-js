eae18f2847cf5873b48448c78334e59c03a2b6fc
Muhammad Rifki Anggareksa <rifkyangga53@gmail.com>
2024-01-30 01:56:31 +0700

    learn react js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Parentbox'>
+      <FotoProduct />
+      <ProductInfo isDiskon='yes' name='Nike Air Jordan 3' category='CASUAL' />
+      
     </div>
   );
+}
+
+function CheckDiskon(props) {
+  const { isDiskon } = props;
+
+  if (isDiskon === 'yes') {
+    return (
+      <p>Diskon 50% off</p>
+    );
+  }
+
+  else if (isDiskon === 'coming') {
+    return (
+      <p>Coming Soon</p>
+    );
+  }
+
+  else {
+    return (
+      <p>Belum ada diskon</p>
+    );
+  }
+}
+
+function FotoProduct() {
+  return(
+  <div className='Foto'>
+      <img src='airjordan.jpg' alt='sepatu' />
+  </div>
+  );
+}
+
+function ProductInfo(props) {
+
+  const {category, name, isDiskon} = props ;
+
+  const benefits = ['Tidak Mudah terkelupas', 'Anti Air', 'Tahan terhadap Api'];
+
+  const listBenefits = benefits.map ((itemBenefit) => 
+    <li>{itemBenefit}</li> 
+  );
+
+  return (
+  <div>
+    <div className='Deskripsi'>
+        <p className='Cate'>{category}</p>
+        <h1 className='Title'>{name}</h1>
+        <p className='Price'>IDR 10.000.000</p>
+        <CheckDiskon isDiskon={isDiskon} />
+        <p className='Info'>
+          One of the most recognizable shoes in AJ Collection, the Air Jordan 3 Retro features lightweight,
+          visible cushioning just like original like from '88. Signature details and materials celebrate the game changing-icon.
+        </p>
+        <ul>
+          {listBenefits}
+        </ul>
+        <a onClick={(e) => inCart(name, e)} href='#'>Add to cart</a>
+
+      </div>
+  </div>
+  );
+}
+
+function inCart (e) {
+  console.log('Membeli produk ' + e);
 }
 
 export default App;
